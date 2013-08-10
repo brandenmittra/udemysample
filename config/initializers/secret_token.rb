7269c0addb5269547c3bdce5dac4a804f6f455fb
Branden Mittra <branden@univrs.co.uk>
2013-08-10 23:42:07 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AAAWebsite::Application.config.secret_token = 'be2fa60b92ed729efdc59985589d4eba2c3a82ed7829bbf504fd3de3a225a412107ddd6eb0de73966253f8e92f1974b3b22822a21d6946530759e95a5d367df4'
